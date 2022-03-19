f608e6fbef05023c9127fe3233d642d9e02fbf6e
Habibur Rahman <habiburrahman-mu@users.noreply.github.com>
2022-03-19 14:21:42 +0600

    added the filter functionalities

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,17 +1,32 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {IProduct} from "./product";
+import {filter} from "rxjs/operators";
 
 @Component({
 	selector: 'pm-products',
-	templateUrl: './product-list.component.html'
+	templateUrl: './product-list.component.html',
+	styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
 	pageTitle: string = 'Product List';
 	imageWidth: number = 50;
 	imageMargin: number = 2;
 	showImage: boolean = false;
-	listFilter: string = 'cart';
-	products: any[] = [
+	private _listFilter: string = '';
+
+	filteredProducts: IProduct[] = [];
+
+	get listFilter(): string {
+		return this._listFilter;
+	}
+
+	set listFilter(value: string) {
+		this._listFilter = value;
+		this.filteredProducts = this.performFilter(value);
+	}
+
+	products: IProduct[] = [
 		{
 			"productId": 2,
 			"productName": "Garden Cart",
@@ -33,7 +48,18 @@ export class ProductListComponent {
 			"imageUrl": "assets/images/hammer.png"
 		}
 	];
+
+	performFilter(filterBy: string): IProduct[] {
+		filterBy = filterBy.toLocaleLowerCase();
+		return this.products.filter((product: IProduct) => product.productName.toLocaleLowerCase().includes(filterBy)
+		);
+	}
+
 	toggleImage(): void {
 		this.showImage = !this.showImage;
+	}
+
+	ngOnInit() {
+		this.filteredProducts = this.products;
 	}
 }
