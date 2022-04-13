e1eb0f6e44b6d16086c14efa26a1ca62d990399d
Habibur Rahman <habiburrahman-mu@users.noreply.github.com>
2022-04-14 03:17:50 +0600

    added product service

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from "@angular/core";
 import {IProduct} from "./product";
 import {filter} from "rxjs/operators";
+import {ProductService} from "./product.service";
 
 @Component({
 	selector: 'pm-products',
@@ -9,13 +10,19 @@ import {filter} from "rxjs/operators";
 })
 
 export class ProductListComponent implements OnInit {
+
+	constructor(private productService: ProductService) {
+	}
+
 	pageTitle: string = 'Product List';
 	imageWidth: number = 50;
 	imageMargin: number = 2;
 	showImage: boolean = false;
 	private _listFilter: string = '';
 
+	products: IProduct[] = [];
 	filteredProducts: IProduct[] = [];
+
 
 	get listFilter(): string {
 		return this._listFilter;
@@ -25,29 +32,6 @@ export class ProductListComponent implements OnInit {
 		this._listFilter = value;
 		this.filteredProducts = this.performFilter(value);
 	}
-
-	products: IProduct[] = [
-		{
-			"productId": 2,
-			"productName": "Garden Cart",
-			"productCode": "GDN-0023",
-			"releaseDate": "March 18, 2021",
-			"description": "15 gallon capacity rolling garden cart",
-			"price": 32.99,
-			"starRating": 4.2,
-			"imageUrl": "assets/images/garden_cart.png"
-		},
-		{
-			"productId": 5,
-			"productName": "Hammer",
-			"productCode": "TBX-0048",
-			"releaseDate": "May 21, 2021",
-			"description": "Curved claw steel hammer",
-			"price": 8.9,
-			"starRating": 4.8,
-			"imageUrl": "assets/images/hammer.png"
-		}
-	];
 
 	performFilter(filterBy: string): IProduct[] {
 		filterBy = filterBy.toLocaleLowerCase();
@@ -60,6 +44,7 @@ export class ProductListComponent implements OnInit {
 	}
 
 	ngOnInit() {
+		this.products = this.productService.getProducts();
 		this.filteredProducts = this.products;
 	}
 
